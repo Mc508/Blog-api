56400d1a20ca03020c43275340de8a85417e4c0e
Mc508 <mihirc508@gmail.com>
2025-06-11 21:57:21 +0530

    get all blogs

--- src/routes/v1/blog.ts
+++ src/routes/v1/blog.ts
@@ -1,14 +1,16 @@
 import { createBlog } from '@/controllers/v1/blog/createBlog';
+import { getAllBlogs } from '@/controllers/v1/blog/getAllBlogs';
 import { authenticate } from '@/middlewares/authenticate';
 import authorize from '@/middlewares/authorize';
 import { uploadBlogBanner } from '@/middlewares/uploadBlogBanner';
 import validationError from '@/middlewares/validationError';
 import { Router } from 'express';
-import { body } from 'express-validator';
+import { body, query } from 'express-validator';
 import multer from 'multer';
 
 const upload = multer();
 const router = Router();
+
 router.post(
   '/',
   authenticate,
@@ -29,5 +31,22 @@ router.post(
   uploadBlogBanner('post'),
   createBlog,
 );
+
+router.get(
+  '/',
+  authenticate,
+  authorize(['admin','user']),
+  query('limit')
+    .optional()
+    .isInt({ min: 1, max: 50 })
+    .withMessage('Limit must be between 1 to 50'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Offset must be greater then 0'),
+  validationError,
+  getAllBlogs,
+);
+
 
 export default router;
